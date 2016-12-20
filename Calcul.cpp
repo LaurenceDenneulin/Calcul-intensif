5120901fbeef6ede5aa37a25f1d517eea9c16365
LaurenceDenneulin <ldenneulin@montesquiou.info>
2016-12-20 09:30:54 +0100

    Ajout de la classe de calcul

---
+++ Calcul.cpp
@@ -0,0 +1,11 @@
+#include "Calcul.h"
+
+Calcul::Calcul()
+{
+    //ctor
+}
+
+Calcul::~Calcul()
+{
+    //dtor
+}
---
+++ Calcul.h
@@ -0,0 +1,14 @@
+#ifndef CALCUL_H
+#define CALCUL_H
+
+
+class Calcul
+{
+    public:
+        Calcul();
+        virtual ~Calcul();
+    protected:
+    private:
+};
+
+#endif // CALCUL_H
