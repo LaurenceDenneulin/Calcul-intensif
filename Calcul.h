16dbe77ce14577cdc5453fc4b57d35a4190e7a94
LaurenceDenneulin <ldenneulin@montesquiou.info>
2016-12-20 09:47:58 +0100

    Tous les objets sont définis

--- Calcul.h
+++ Calcul.h
@@ -1,6 +1,15 @@
 #ifndef CALCUL_H
 #define CALCUL_H
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+const int Kmax = 1000;
+const double m1 = 10.19357;
+const double s1 = 0.3022616;
+const double m2 = 11.795;
+const double s2 = s1;
 
 class Calcul
 {
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include"Calcul.h"
+
+int main()
+{
+    // Déclaration des constante
+
+    // Il faudra faire en parallèle :
+
+    int taxmax(0);
+
+    // le SeedRandim[1234]
+    //printf ("First number: %d\n", rand()%100);
+    srand (time(NULL));
+    //printf ("Random number: %d\n", rand()%100);
+    srand (1234);
+    int r=rand() ;
+    //printf ("Again the first number: %d\n", rand()%100);
+    std::cout <<" r vaut :"<< r << std::endl;
+
+    double S [2][2];
+    S[0][0]=s1*s1;
+    S[0][1]=r*s1*s2;
+    S[1][0]=r*s1*s2;
+    S[1][1]=s2*s2;
+
+    std::cout << " S vaut :" << S<< std::endl;
+
+
+
+}
